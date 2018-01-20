9685eaea837d05a08c9de29f4685a8292b04cf94
genkidesuka <jonathan-diangana@live.fr>
2018-01-20 18:08:38 +0100

    samedi 20 janvier 2018, 18:08:38 (UTC+0100)

--- synch.sh
+++ synch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Synchronize with git's server
+# usage: $ ./synch.sh <file or folder>
 git status
 
 while true; do
     read -p "Do you wish to synchronize the folder with git? (y/n)?" ans
     case $ans in
         [Yy]* )
-            git add .;
+            git add $1;
             git commit -m "`date`";
             git push;
             exit ;;
